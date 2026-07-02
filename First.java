955462408e509ead5d38d6f4bbdc611dda919524
armainsheikh73 <armainsheikh7373@gmail.com>
2026-07-02 20:16:39 +0530

    Day02

---
+++ First.java
@@ -0,0 +1,9 @@
+class Test 
+{
+public static void main(String args[])
+{
+System.out.println("fruits name are ");
+System.out.println("apple,mango,orange,grapes,strawberry,etc");
+}
+
+}
---
+++ T.java
@@ -0,0 +1,9 @@
+class T
+{
+public static void main(String a[])
+{
+System.out.println("Hello Armain..!");
+system.out.println("this is my first java program....");
+}
+
+}
